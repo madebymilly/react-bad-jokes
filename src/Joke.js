5883e4f48ef667dce9aa7cc203bf0b18c0857de3
Milly Uylenbroek <m.uylenbroek@eagerly.nl>
2021-09-13 20:01:02 +0200

    added reusable fetchJokes function

--- src/Joke.js
+++ src/Joke.js
@@ -1,10 +1,78 @@
 import React, { Component } from 'react'
+import './Joke.css'
 
 export default class Joke extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       
+    }
+    this.handleScoreUp = this.handleScoreUp.bind(this)
+    this.handleScoreDown = this.handleScoreDown.bind(this)
+    this.getColor = this.getColor.bind(this)
+    this.getEmoticon = this.getEmoticon.bind(this)
+  }
+
+  handleScoreUp() {
+    this.props.changeScore(this.props.joke.id, 1)
+  }
+
+  handleScoreDown() {
+    this.props.changeScore(this.props.joke.id, -1)
+  }
+
+  getColor() {
+    if (this.props.joke.score >= 15) {
+      return "#4CAF50";
+    } else if (this.props.joke.score >= 12) {
+      return "#8BC34A";
+    } else if (this.props.joke.score >= 9) {
+      return "#CDDC39";
+    } else if (this.props.joke.score >= 6) {
+      return "#FFEB3B";
+    } else if (this.props.joke.score >= 3) {
+      return "#FFC107";
+    } else if (this.props.joke.score >= 0) {
+      return "#FF9800";
+    } else {
+      return "#f44336";
+    }
+  }
+  
+  getEmoticon() {
+    if (this.props.joke.score >= 15) {
+      return "em em-rolling_on_the_floor_laughing";
+    } else if (this.props.joke.score >= 12) {
+      return "em em-laughing";
+    } else if (this.props.joke.score >= 9) {
+      return "em em-smiley";
+    } else if (this.props.joke.score >= 6) {
+      return "em em-slightly_smiling_face";
+    } else if (this.props.joke.score >= 3) {
+      return "em em-neutral_face";
+    } else if (this.props.joke.score >= 0) {
+      return "em em-confused";
+    } else {
+      return "em em-angry";
+    }
+  }
+
   render() {
+    const { joke } = this.props;
     return (
-      <div>
-        Joke...
+      <div className="Joke">
+        <div className="Joke-buttons">
+          <i className="Joke-score-up far fa-thumbs-up" onClick={this.handleScoreUp}></i>
+          <div className="Joke-score" style={{borderColor: this.getColor()}}>{joke.score}</div>
+          <i className="Joke-score-down far fa-thumbs-down" onClick={this.handleScoreDown}></i>
+        </div>
+        <div className="Joke-text">
+          {joke.joke}
+        </div>
+        <div className="Joke-emoticon">
+          <i className={this.getEmoticon()}></i>
+        </div>
       </div>
     )
   }
--- src/JokeList.js
+++ src/JokeList.js
@@ -1,15 +1,82 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import './JokeList.css'
 import Joke from './Joke'
 
 export default class JokeList extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       jokes: [],
+       jokesLoading: false
+    }
+
+    this.changeScore = this.changeScore.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+    this.fetchJokes = this.fetchJokes.bind(this)
+  }
+
+  componentDidMount() {
+    this.fetchJokes();
+  }
+
+  changeScore(jokeID, amount) {
+    let tempJokes = this.state.jokes.map(joke =>
+      joke.id === jokeID
+        ? { ...joke, score: joke.score + amount }
+        : joke
+    );
+    this.setState({
+      jokes: tempJokes
+    })
+  }
+
+  handleClick() {
+    this.fetchJokes();
+  }
+
+  async fetchJokes() {
+    this.setState({ loadingJokes: true });
+    let newJokes = [];
+    while(newJokes.length < 10) {
+      let response = await axios.get("https://icanhazdadjoke.com/", {
+        headers: { Accept: "application/json" }
+      });
+      const newJoke = { id: response.data.id, joke: response.data.joke, score: 0 }
+      newJokes = [...newJokes, newJoke];
+    }
+    this.setState(prevState => ({
+      jokes: [...prevState.jokes, ...newJokes],
+      loadingJokes: false
+    }));
+  }
+  
   render() {
+    if (this.state.loadingJokes) {
+      return (
+        <div className='JokeList-spinner'>
+          <i className='far fa-8x fa-laugh fa-spin' />
+          <h1 className='JokeList-title'>Loading...</h1>
+        </div>
+      );
+    }
     return (
       <div className="JokeList">
-        jokelist...
-        <Joke />
-        <Joke />
-        <Joke />
+        <div className='JokeList-sidebar'>
+          <h1 className='JokeList-title'>
+            <span>Bad</span> Jokes
+          </h1>
+          <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' alt="smiley"/>
+          <button className='JokeList-getmore' onClick={this.handleClick}>
+            Fetch More Jokes
+          </button>
+        </div>
+        <div className='JokeList-jokes'>
+          {this.state.jokes.map(joke =>
+            <Joke key={joke.id} joke={joke} changeScore={this.changeScore} />
+          )}
+        </div>
       </div>
     )
   }
